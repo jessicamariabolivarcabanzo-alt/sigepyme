e012b289c2bc81a6025f362722e15f66db453723
jesica <jessicabolivar@gmail.com>
2026-03-26 21:42:23 -0500

    fix allowed hosts

--- sigepyme/settings.py
+++ sigepyme/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p(o6v2*a26r(s*5b_awgl^4yms+hvyx8mw6orq+q3z)w2#(_%c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sigepyme.onrender']
 
 
 # Application definition
